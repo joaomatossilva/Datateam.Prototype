d3a101a2f26114c83441debc4b9c3bcf397139f4
Joao Matos Silva <kappy@acydburne.com.pt>
2013-01-10 21:00:40 +0000

    Added project and content

---
+++ DataTeam/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataTeam.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UserControl()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                ViewBag.UserName = User.Identity.Name;
+                return PartialView("_UserControl");
+            }
+            return new EmptyResult();
+        }
+
+    }
+}
---
+++ DataTeam/Controllers/ExampleLayoutsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BootstrapMvcSample.Controllers
+{
+    public class ExampleLayoutsController : Controller
+    {
+        public ActionResult Starter()
+        {
+            return View();
+        }
+
+        public ActionResult Marketing()
+        {
+            return View();
+        }
+
+        public ActionResult Fluid()
+        {
+            return View();
+        }
+
+        public ActionResult Narrow()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(FormCollection collection)
+        {
+            FormsAuthentication.SetAuthCookie(collection["email"], true);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult StickyFooter()
+        {
+            return View("TBD");
+        }
+
+        public ActionResult Carousel()
+        {
+            return View("TBD");
+        }
+    }
+}
